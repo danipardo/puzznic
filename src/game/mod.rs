5735314137a343aa318f0da6bd7ed0156002e1fb
Dani Pardo <dani.pardo@inmensys.com>
2020-04-16 19:10:50 +0200

    Adds the brick texture as an optional

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -5,42 +5,34 @@ use sdl2::rect::Rect;
 use std::path::Path;
 use sdl2::render::TextureCreator;
 
-const scale :u32 = 2;
+const scale: u32 = 2;
 
 #[derive(Debug, Copy, Clone)]
-pub struct Tile{
-
+pub struct Tile {
     pub c: char
-
 }
 
-pub struct TileMap{
-
+pub struct TileMap {
     pub map: Vec<Vec<Tile>>
-
 }
 
 pub struct GraphicsSubsystem {
 
     pub canvas: Canvas<Window>,
-    pub brick_texture :  Texture<'static>,
+    pub brick_texture: Option<Texture<'static>>,
     pub event_pump: sdl2::EventPump,
-    pub texture_creator:  TextureCreator<sdl2::video::WindowContext>
+    pub texture_creator: TextureCreator<sdl2::video::WindowContext>,
 }
 
 impl GraphicsSubsystem {
-
-    pub fn finished( &self) -> bool {
-
+    pub fn finished(&self) -> bool {
         return false;
-
     }
-    pub fn clear(&self){
+    pub fn clear(&self) {
 //        self.canvas.clear();
-
     }
-    pub fn present(&self){
-  //      self.canvas.present();
+    pub fn present(&self) {
+        //      self.canvas.present();
     }
 
     // pub fn getBrickTexture(&self) -> &Texture {
@@ -53,91 +45,76 @@ impl GraphicsSubsystem {
     //     &brick_texture
     // }
 }
-   pub fn init() -> GraphicsSubsystem {
 
+pub fn init() -> GraphicsSubsystem {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
- 
+
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
         .unwrap();
- 
-    let mut canvas = window.into_canvas().build().unwrap();
 
-//    let texture_creator = canvas.texture_creator();
-
-    // init the backgorund bricks
     let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("img/bg1.bmp")).unwrap();
+    let canvas = window.into_canvas().build().unwrap();
+
     let texture_creator = canvas.texture_creator();
+//
+//     // init the backgorund bricks
+//     let texture_creator = canvas.texture_creator();
+//
 
-   let brick_texture = texture_creator.create_texture_from_surface(&temp_surface)
-       .map_err(|e| e.to_string()).unwrap();
-
-   
+    //   let brick_texture =  texture_creator.create_texture_from_surface(&temp_surface).unwrap();
+//
+//
     let event_pump = sdl_context.event_pump().unwrap();
 
     GraphicsSubsystem {
-         canvas: canvas,
-         brick_texture: brick_texture, 
-         event_pump: event_pump,
-         texture_creator:  texture_creator
-        }
-
-
+        canvas: canvas,
+        event_pump: event_pump,
+        brick_texture: None,
+        texture_creator: texture_creator,
     }
+}
 
-pub fn load_map( level : &String) -> TileMap {
-
-
+pub fn load_map(level: &String) -> TileMap {
     let mut map: Vec<Vec<Tile>> = vec![];
 
-     let lines = level.split("\n");
+    let lines = level.split("\n");
 
-     for line in lines {
-
+    for line in lines {
         let mut s1 = vec![];
         for c in line.chars() {
-
-            let t: Tile = Tile{c};
+            let t: Tile = Tile { c };
 
             s1.push(t);
         }
 
         map.push(s1);
-
-     }
-    TileMap{map}
-
+    }
+    TileMap { map }
 }
 
 
+pub fn draw_background_tiles(gs: &mut GraphicsSubsystem) {
+    const WIDTH: u32 = 16;
+    const HEIGHT: u32 = 8;
 
-pub fn draw_background_tiles(gs:  &mut GraphicsSubsystem){
 
-   const WIDTH :u32 = 16;
-   const HEIGHT :u32 = 8;
-   
+    //  let texture = gs.brick_texture;
+    let source_rect = Rect::new(0, 0, 12, 10);
 
- //  let texture = gs.brick_texture;
-   let source_rect = Rect::new(0, 0, 12, 10);
+    for x in 0..25 {
+        for y in 0..38 {
+            let dest_rect = Rect::new((x * WIDTH * scale) as i32,
+                                      (y * HEIGHT * scale) as i32,
+                                      WIDTH * scale,
+                                      HEIGHT * scale);
 
-   for x  in 0..25 {
-    for y in 0..38 {
-
-        let dest_rect = Rect::new( (x*WIDTH*scale) as i32,
-            (y*HEIGHT*scale) as i32,
-            WIDTH *scale,
-            HEIGHT * scale);
-        
             // gs.canvas.copy_ex(&texture,
             // Some(source_rect),
             //  Some(dest_rect),
             //  0.0, None, false, false).unwrap();
-
+        }
     }
-}
-
-
-
 }
